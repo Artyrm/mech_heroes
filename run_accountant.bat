2930e18e69230e1176e99e361a9329f5cafcc664
Artyrm <ksotar@yandex.ru>
2026-04-29 02:55:05 +0300

    Report updated 29.04 02:55

--- run_accountant.bat
+++ run_accountant.bat
@@ -1,6 +1,4 @@
 @echo off
-cd /d "g:\Video\!Медведи\Mech Heroes\Клан Орки\accountant_bot\clan_monitor"
-
-echo [%date% %time%] START >> debug_log.txt
-C:\tools\Anaconda3\python.exe clan_accountant.py >> debug_log.txt 2>&1
-echo [%date% %time%] END >> debug_log.txt
+chcp 65001 >nul
+cd /d "%~dp0clan_monitor"
+C:\tools\Anaconda3\python.exe clan_accountant.py
